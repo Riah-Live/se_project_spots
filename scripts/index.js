047f9a3e898d331956799189404d4eb0219bfd14
MARIAH ASH <mariah@theartsprojectinc.org>
2025-09-08 14:06:34 -0400

    Correct capitalization for JavaScript syntax

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,10 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
 });
@@ -10,14 +14,10 @@ editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-const NewPostBtn = document.querySelector(".profile__add-btn");
-const newPostModal = document.querySelector("#new-post-modal");
-const NewPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-
-NewPostBtn.addEventListener("click", function () {
+newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
 
-NewPostCloseBtn.addEventListener("click", function () {
+newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
