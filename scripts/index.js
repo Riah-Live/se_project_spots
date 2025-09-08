892013288e439691b52595a19262b66df20e6dec
MARIAH ASH <mariah@theartsprojectinc.org>
2025-09-08 13:55:42 -0400

    Add document query selectors and event listeners with JavaScript to open and close modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
-console.log("hey rah");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+const NewPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const NewPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+NewPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+NewPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
